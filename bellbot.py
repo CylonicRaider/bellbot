69299573d8404e61877f7e4ca9d1fb89f2c234fc
CylonicRaider <CylonicRaider@users.noreply.github.com>
2020-12-24 00:14:47 +0100

    Add message matching function

--- bellbot.py
+++ bellbot.py
@@ -5,6 +5,16 @@
 A Euphoria bot that watches for the disappearance of a user.
 """
 
+def match_check(msg, check):
+    if check['type'] == 'nick':
+        return msg.sender.name == check['nick']
+    elif check['type'] == 'nick-regex':
+        return bool(check['regex'].search(msg.sender.name))
+    elif check['type'] == 'uid':
+        return msg.sender.id == check['id']
+    else:
+        raise RuntimeError('Unrecognized check %r' % (check['type'],))
+
 def main():
     raise NotImplementedError
 
