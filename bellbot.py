dd1d5af36ba1c545e612a695640ab87d8fab142e
CylonicRaider <CylonicRaider@users.noreply.github.com>
2020-12-23 23:37:26 +0100

    Commence main source file

---
+++ bellbot.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: ascii -*-
+
+"""
+A Euphoria bot that watches for the disappearance of a user.
+"""
+
+def main():
+    raise NotImplementedError
+
+if __name__ == '__main__': main()
